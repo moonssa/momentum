354159187d6732228a41c1524bba897e0503a484
oborimoon <oborimoon@gmail.com>
2021-12-22 15:33:00 +0900

    weather

--- js/clock.js
+++ js/clock.js
@@ -5,9 +5,10 @@ function getClock() {
     const hours = String(date.getHours()).padStart(2,"0");
     const minutes = String(date.getMinutes()).padStart(2,"0");
     const seconds = String(date.getSeconds()).padStart(2,"0");
-    console.log(typeof hours);
+
     clock.innerText = `${hours}:${minutes}:${seconds}`
 }
 
 getClock();
 setInterval(getClock, 1000);
+
