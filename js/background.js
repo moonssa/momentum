f4d1919fb2a5a0b140e3dd83e96a61076e63b205
oborimoon <oborimoon@gmail.com>
2021-12-30 10:18:20 +0900

    apply css styling

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const images = ["1.jpg", "2.jpg", "3.jpg"];
+const images = ["1.jpg", "2.jpg", "3.jpg", "4.webp"];
 
 chosenImage = images[Math.floor(Math.random()*images.length)];
 
@@ -7,6 +7,7 @@ bgImage.src = `img/${chosenImage}`
 
 console.log(bgImage);
 
+//const background = document.querySelector("#background");
 document.body.appendChild(bgImage);
 
 
